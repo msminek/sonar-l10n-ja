a3463207a8bc29a8087786314b296f8478dbf0f7
Mineo Kasai <42196206+msminek@users.noreply.github.com>
2022-06-24 19:31:32 +0900

    - Merge skelton from xuhusheng/sonar-l10n-zh@d658c0f.

--- src/test/java/org/sonar/plugins/l10n/JapanesePackPluginTest.java
+++ src/test/java/org/sonar/plugins/l10n/JapanesePackPluginTest.java
@@ -1,7 +1,7 @@
 /*
  * L10n :: Japanese Pack
- * Copyright (C) 2012 Yoshiyuki NISHINAKA
- * yoshiyuki.nishinaka@gmail.com
+ * Copyright (C) 2014-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com 
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -13,20 +13,44 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.sonar.plugins.l10n;
 
 import org.junit.Test;
+import org.junit.Assert;
+import org.sonar.api.internal.PluginContextImpl;
+import org.sonar.api.internal.SonarRuntimeImpl;
+import org.sonar.api.Plugin;
+import org.sonar.api.SonarEdition;
+import org.sonar.api.SonarQubeSide;
+import org.sonar.api.SonarRuntime;
+import org.sonar.api.utils.Version;
 import org.sonar.test.i18n.I18nMatchers;
 
 public class JapanesePackPluginTest {
 
+    // https://jira.sonarsource.com/browse/SONAR-7226
     @Test
     public void bundles_should_be_up_to_date() {
+        // Skip test, because of merge conflict in SonarQube-6.1
+        // https://github.com/SonarSource/sonarqube/commit/50c03de3431007269b0966a8fdf1fe032c9521f6
         I18nMatchers.assertBundlesUpToDate();
     }
 
+    // coverage
+    @Test
+    public void testjapanesePackPlugin() {
+        JapanesePackPlugin japanesePackPlugin = new JapanesePackPlugin();
+
+        String pluginName = japanesePackPlugin.toString();
+        Assert.assertEquals("japanesePackPlugin", pluginName);
+
+        SonarRuntime runtime = SonarRuntimeImpl.forSonarQube(Version.create(8, 5),
+                SonarQubeSide.SCANNER, SonarEdition.COMMUNITY);
+        Plugin.Context context = new PluginContextImpl.Builder().setSonarRuntime(runtime).build();
+        japanesePackPlugin.define(context);
+    }
 }
